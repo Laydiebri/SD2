91ba5892dd59615b638021ebd4e76a321c51b100
Zabrina Osborne <Laydiebri24@hotmail.com>
2021-04-25 22:29:53 +0100

    working

---
+++ db_test.js
@@ -0,0 +1,41 @@
+// Import SQLite library.
+// Use verbose mode to give more detailed error outputs
+const sqlite3 = require("sqlite3").verbose();
+
+// Connect to the database.
+// Function is callback when connection completed.
+// err is any error message that occurs
+let db = new sqlite3.Database("studentz.db", function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to studentz database.");
+});
+
+// *** NEW CODE STARTS HERE ***
+// SQL query to run
+var sql = "SELECT * FROM Modules";
+
+// Execute SQL query, and run function on every row returned.
+db.each(sql, function(err, row) {
+    // If error display
+    if (err) {
+        return console.error(err.message);
+    }
+    // Print the code column and name column from row seperated by a tab \t
+    console.log(row.code + "\t" + row.name);
+});
+
+// *** NEW CODE ENDS HERE ***
+
+// Close the database connection.
+// Always close the connection when you are finished with it.
+// Function is callback when connection is closed.
+db.close(function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Closed connection to studentz database.");
+});
